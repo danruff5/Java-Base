2728ce24414eec485ced3902090584c95a4caab3
Daniel <dckrauskopf@gmail.com>
2013-12-03 17:50:27 -0500

    Create monsterTest.java
    stuf from boolean

---
+++ monsterTest.java
@@ -0,0 +1,18 @@
+package monster;
+
+public class monsterTest {
+
+    public static void main(String[] args) {
+        Monster Kirk = new Monster();
+
+        System.out.println("name: " + Kirk.randName());
+        System.out.println(Kirk.getAttack() + " is the attack");
+        System.out.println(Kirk.getHealth() + " is the health add the armor " + Kirk.getArmor());
+        Kirk.printBattleCry();
+        System.out.println(Kirk.getxPos() + " is the xpos");
+        System.out.println(Kirk.getyPos() + " this is the ypos");
+        System.out.println(Kirk.getdead());
+        System.out.println("waht is your attack");
+        //Scanner.class
+    }
+}
